b5541dd0481266c04bb14bc79e88d1b58778d0c6
CrisKoch <cristinak.iftm@gmail.com>
2020-05-08 20:27:20 -0300

    Consolidado de renda

--- src/imposto.java
+++ src/imposto.java
@@ -7,8 +7,8 @@ public class imposto {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double salario, servico, capital, gastosMedicos, gastosEducacionais;
-		
+		double salario, servico, capital, gastosMedicos, gastosEducacionais, impSalario, impServicos, impCapital, impostoTotal;
+		double salarioMensal, deducao, impostoDevido;
 		
 		//Leitura dos dados
 		
@@ -23,7 +23,33 @@ public class imposto {
 		System.out.print("Gastos educacionais: ");
 		gastosEducacionais = sc.nextDouble();
 		
+		//Calculos
 		
+		salarioMensal = salario / 12;
+		if(salarioMensal < 3000.00) {
+			impSalario = 0.00;
+		}
+			else if(salarioMensal >= 3000.00 && salarioMensal <= 5000.00) {
+				impSalario = (salario * 10) / 100;
+			}
+			else {
+				impSalario = (salario * 10) / 100;
+			}
+		impServicos = (servico * 15) / 100;
+		impCapital = (capital * 20) / 100;
+		
+		impostoTotal = impSalario + impServicos + impCapital;
+		deducao = gastosMedicos + gastosEducacionais;
+		
+		
+		//Relatorio
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA:");
+		System.out.println();
+		System.out.println("CONSOLIDADO DE RENDA:");
+		System.out.printf("Imposto sobre salário = %.2f%n", impSalario);
+		System.out.printf("Imposto sobre serviços = %.2f%n", impServicos);
+		System.out.printf("Imposto sobre ganho de capital = %.2f%n", impCapital);
 		
 		sc.close();
 	}
