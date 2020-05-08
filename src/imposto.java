e9c9d33df705c6609b6e9fe9473678f05e902677
CrisKoch <cristinak.iftm@gmail.com>
2020-05-08 19:41:15 -0300

    Leitura dos dados

--- src/imposto.java
+++ src/imposto.java
@@ -1,9 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class imposto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double salario, servico, capital, gastosMedicos, gastosEducacionais;
+		
+		
+		//Leitura dos dados
+		
+		System.out.print("Renda anual com salário: ");
+		salario = sc.nextDouble();
+		System.out.print("Renda anual com prestação de serviços: ");
+		servico = sc.nextDouble();
+		System.out.print("Renda anual com ganho de capital: ");
+		capital = sc.nextDouble();
+		System.out.print("Gastos médicos: ");
+		gastosMedicos = sc.nextDouble();
+		System.out.print("Gastos educacionais: ");
+		gastosEducacionais = sc.nextDouble();
+		
+		
+		
+		sc.close();
 	}
 
 }
